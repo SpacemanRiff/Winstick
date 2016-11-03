75b16fecb8e2b4b5fe4609c6fc6bbb3983df5b36
Scott Troutman <troutmansm@gmail.com>
2016-11-03 15:09:07 -0500

    started adjacency check code

--- WinStick2/Assets/Scripts/LevelGenerator.cs
+++ WinStick2/Assets/Scripts/LevelGenerator.cs
@@ -8,10 +8,14 @@ public class LevelGenerator : MonoBehaviour {
     public int height;
     public int averageRoomSize;
     private GameObject[,] tiles;
+    private bool[,] tilesGenerated;
 
 	// Use this for initialization
 	void Start () {
         tiles = new GameObject[width, height];
+        tilesGenerated = new bool[width, height];
+        for (int i = 0; i < width; i++) { for (int j = 0; j < height; j++) { tilesGenerated[i, j] = false; } }
+
         int numRooms = Mathf.CeilToInt(Random.value * 4) + 2;
         Vector2[,] rooms = new Vector2[numRooms, 2]; //(w,h),(x,y)
         
@@ -19,36 +23,60 @@ public class LevelGenerator : MonoBehaviour {
         {
             rooms[i, 0] = new Vector2(Mathf.CeilToInt(RandomNormal(averageRoomSize)), Mathf.CeilToInt(RandomNormal(averageRoomSize)));
             rooms[i, 1] = new Vector2((Mathf.CeilToInt(Random.value * (width - rooms[i, 0].x))) % width, (Mathf.CeilToInt(Random.value * (height - rooms[i, 0].y))) % height);
-            Debug.Log(rooms[i, 0].x + " " + rooms[i, 0].y + " " + rooms[i, 1].x + " " + rooms[i, 1].y);
+            Debug.Log("Room " + i + ": " + rooms[i, 0].x + " " + rooms[i, 0].y + " " + rooms[i, 1].x + " " + rooms[i, 1].y);
         }
 
-        for (int i = 0; i < numRooms; i++)
+        GenerateRooms(rooms);
+        AttachIsolatedRooms(rooms);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    private void GenerateRooms(Vector2[,] rooms)
+    {
+        for (int i = 0; i < rooms.GetLength(0); i++)
         {
             int[] size = { (int)rooms[i, 0].x, (int)rooms[i, 0].y };
             int[] coords = { (int)rooms[i, 1].x, (int)rooms[i, 1].y };
-            for(int x = 0; x < size[0]; x++)
+            for (int x = 0; x < size[0]; x++)
             {
                 for (int y = 0; y < size[1]; y++)
                 {
                     tiles[coords[0] + x, coords[1] + y] = (GameObject)Instantiate(terrain[0]);
                     tiles[coords[0] + x, coords[1] + y].transform.position = new Vector2(coords[0] + x, coords[1] + y);
+                    tilesGenerated[coords[0] + x, coords[1] + y] = true;
                 }
             }
         }
-        /*for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                tiles[i, j] = (GameObject)Instantiate(terrain[0]);
-                tiles[i, j].transform.position = new Vector2(i, j);
-            }
-        }*/
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void AttachIsolatedRooms(Vector2[,] rooms)
+    {
+        for (int i = 0; i < rooms.GetLength(0); i++)
+        {
+            bool adjacent = false;
+            int[] size = { (int)rooms[i, 0].x, (int)rooms[i, 0].y };
+            int[] coords = { (int)rooms[i, 1].x, (int)rooms[i, 1].y };
+            for(int x = 0; x < size[0] && !adjacent; x++)
+            {
+                if(!(coords[1]-1 < 0))
+                {
+                    adjacent = tilesGenerated[x + coords[0], coords[1] - 1];
+                }
+            }
+            for (int y = 0; y < size[1] && !adjacent; y++)
+            {
+                if (!(coords[0] - 1 < 0))
+                {
+                    adjacent = tilesGenerated[coords[0] - 1, y + coords[1]];
+                }
+            }
+        }
+    }
 
     private float RandomNormal(int mean)
     {
