0ed8c66241fce69cf59b0c3e57c9a02d73630409
Scott Troutman <troutmansm@gmail.com>
2016-11-11 22:02:42 -0600

    not sword. now gun

--- WinStick2/Assets/Scripts/PlayerController.cs
+++ WinStick2/Assets/Scripts/PlayerController.cs
@@ -2,30 +2,49 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-    private Animator ac;
-    private Rigidbody2D rb;
     public float speed;
-    
-	// Use this for initialization
-	void Start () {
-        ac = GetComponent<Animator>();
+    public float projectileSpeed; //this should be temporary. remove when a script for bullets is created
+    public float bulletKnockback;
+    public float rateOfFire;
+    public GameObject gun;
+    public GameObject bulletPrefab;
+
+    private Rigidbody2D rb;
+    private int timeUntilNextShot;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
+        timeUntilNextShot = 0;
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
-
         Vector3 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(direction.y, direction.x);
-        rb.AddForce((transform.right*Input.GetAxis("Vertical") + transform.up*Input.GetAxis("Horizontal")).normalized * speed, ForceMode2D.Force);
-        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+        gun.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * speed, ForceMode2D.Force);
+
         Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && timeUntilNextShot <= 0)
         {
-            Debug.Log("Swing");
-            ac.SetTrigger("Swing");
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab);
+            Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            bullet.transform.position = gun.transform.position + new Vector3(Mathf.Cos(angle) * 0.5f, Mathf.Sin(angle) * 0.5f, 0);
+            bullet.transform.rotation = gun.transform.rotation;
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * projectileSpeed;
+            Destroy(bullet, 2);
+
+            rb.AddForce((gun.transform.right * bulletKnockback) * -1, ForceMode2D.Impulse);
+
+            timeUntilNextShot = (int)(50.0 / rateOfFire);
+        }
+
+        if(timeUntilNextShot > 0)
+        {
+            timeUntilNextShot--;
         }
     }
 }
